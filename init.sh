77590eaba10e334cc249cfaeb2f3f1ac910f64b8
DmitryErokhin <dimaan333@gmail.com>
2020-08-07 18:09:26 +0300

    change etc

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ln -sf /home/dmitry/stepic_web_project/etc/nginx.conf /etc/nginx/#sites-enabled/test.conf
+#sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/mysql start
+
